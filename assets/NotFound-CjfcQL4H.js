8b2a3ceb3e9a6def8d7d24d6ad7389505985f70d
Kevin Bardou <kbardou86@gmail.com>
2025-01-01 11:08:42 +0100

    adding dist

---
+++ assets/NotFound-CjfcQL4H.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,a as o}from"./index-CrwIgr46.js";const c={},n={class:"text-8xl"};function s(r,_){return o(),e("h1",n,"404 NotFound")}const d=t(c,[["render",s]]);export{d as default};
