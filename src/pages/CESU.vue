ec2388c3df53ed4c8f87cec3b27d01cd3f3c0725
Kevin Bardou <kbardou86@gmail.com>
2025-01-04 21:43:38 +0100

    adding dist

---
+++ src/pages/CESU.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import {onMounted} from "vue";
+import {useHead} from "@vueuse/head";
+
+onMounted(() => {
+  useHead({
+    title: 'Frédéric Bardou | Service à domicile'
+  })
+});
+</script>
+
+<template>
+  <h1 class="text-8xl font-title">Accueil Test</h1>
+</template>
+
+<style scoped>
+
+</style>
