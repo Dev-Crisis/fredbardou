900ad81c50a8a00020517a4bac36013d1e0807ab
Kevin Bardou <kbardou86@gmail.com>
2025-01-08 21:59:19 +0100

    adding dist

---
+++ dist/assets/NotFound-DCd3clca.js
@@ -0,0 +1,1 @@
+import{m as o,c as t,o as e}from"./index-B2gP0Zxa.js";const c={},n={class:"text-8xl"};function s(r,_){return e(),t("h1",n,"404 NotFound")}const d=o(c,[["render",s]]);export{d as default};
